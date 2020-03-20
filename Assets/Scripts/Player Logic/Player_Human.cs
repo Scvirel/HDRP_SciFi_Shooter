e140f2e176e167a83f2cd84f4ca85c6006c3b57d
Vladislav Singayevsky <singayevskyvladislav@gmail.com>
2020-03-20 16:02:49 +0300

    Basic interface

---
+++ Assets/Scripts/Player Logic/Player_Human.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac6897b40ff464c232cdc2d6a1dc6eca8a0c9a492d0c29f5055bd0b9a8a7df1f
+size 3038
--- Assets/Scripts/Player_Human.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:725d47b2dedd01205aea4c75f2db40eb3ecf28742ac9f0a53bd31fb043e012c0
-size 3046
