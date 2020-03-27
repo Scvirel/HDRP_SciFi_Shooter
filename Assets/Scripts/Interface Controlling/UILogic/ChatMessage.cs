16c0384d05285a1ebc606e520b5f95d50dd2ee43
Vladislav Singayevsky <singayevskyvladislav@gmail.com>
2020-03-28 01:40:07 +0300

    Chat basic

---
+++ Assets/Scripts/Interface Controlling/UILogic/ChatMessage.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1126ff14a8cc10c85b3157771a28bdb411b4db8813da1ce180091f43d5a46899
+size 662
--- Assets/Scripts/Players/Player_Human.cs
+++ Assets/Scripts/Players/Player_Human.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e49fb9ef5cb756623cceccb242841c35a2d2ee1dffc8f3bc69a1f2f021ef6c7
-size 5778
+oid sha256:b8bc773ad46f64f027e5e5e896e65239edb11205c63c6e1d113b1739d802a4be
+size 6150
